3fd1d093a72baef2c07f858f51d1079d6344cab9
Polina <polinaleeeen@gmail.com>
2017-06-28 08:49:56 +0000

    changegemfile

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  
+  before_action :set_article, only: [:show,:edit,:update,:destroy]
+  
+  
+  
   def index 
       @articles = Article.all
   end
@@ -8,14 +13,46 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect 
     @article  = Article.new(article_params)
-    if 
-      @article.save
+    if @article.save
+      flash[:notice] = "post added"
+      redirect_to articles_path
     else 
       render "new"
+      
     end
   end
+  
+  def show
+    
+  end
+  
+  def edit
+   
+  end
+  
+  def update
+   
+    if @article.update(article_params)
+      flash[:notice] = "post updated"
+      redirect_to article_path(@article)
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+     
+     @article.destroy
+     flash[:notice] = "Ой все ("
+     redirect_to articles_path
+  end
+  
   private
   def article_params    
     params.require(:article).permit(:title, :description)
   end  
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
 end
